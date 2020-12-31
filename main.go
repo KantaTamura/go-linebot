62dc5afe87773d0b7a795acd3fd69b5b3a9569f8
PhiZero <kanta03085@outlook.jp>
2020-12-31 21:04:51 +0900

    HTTPサーバの立ち上げ

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	// ハンドラの登録
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	msg := "Hello World!!!!"
+	_, _ = fmt.Fprintf(w, msg)
 }
