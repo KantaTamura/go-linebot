01264059eee26015469552d32d29a05b72c698f2
PhiZero <kanta03085@outlook.jp>
2020-12-31 21:51:41 +0900

    オウムがえしBOT

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
 	// ハンドラの登録
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
 
 	fmt.Println("http://localhost:8080 で起動中...")
 	// HTTPサーバを起動
@@ -18,4 +21,42 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	msg := "Hello World!!!!"
 	_, _ = fmt.Fprintf(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	// BOTを初期化
+	bot, err := linebot.New(
+		"33924efdfda6df94e64e2ec1acca2df3",
+		"S6udGVYO1oUA6HtzG9mu2NZ14AR9cPHamWQ99gMUhW8IkG9NUBpxju6zNYojFw6RMLbzAJySmavqSzufuaVnmOnQ5fBVughUBbJvB5sucRQjl/Qs1F/zBMUbZraws28N8ySxEHVkwbVTPtGwort1jwdB04t89/1O/w1cDnyilFU=",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// リクエストからBOTのイベントを取得
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		// イベントがメッセージ受信だった場合
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
+			case *linebot.TextMessage:
+				replyMessage := message.Text
+				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+				if err != nil {
+					log.Print(err)
+				}
+			}
+			//　他にもスタンプや画像、位置情報など色々受信可能
+		}
+	}
+
 }
